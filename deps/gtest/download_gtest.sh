0892d1b8e3a877d47ab537067044daf6685cb568
PekeDevil <47299835+PekeDevil@users.noreply.github.com>
2021-07-02 10:07:30 +0200

    Update download_gtest.sh

--- deps/gtest/download_gtest.sh
+++ deps/gtest/download_gtest.sh
@@ -7,7 +7,7 @@ if [[ $VERSION = "" ]]; then
 fi
 
 GTEST_VERSION=googletest-release-${VERSION}.zip
-GTEST_LINK=https://github.com/google/googletest/releases/tag/release-${VERSION}.zip
+GTEST_LINK=https://github.com/google/googletest/archive/refs/tags/release-${VERSION}.zip
 
 if [[ ! -f ${GTEST_VERSION} ]]; then
     wget -O ${GTEST_VERSION} ${GTEST_LINK}
