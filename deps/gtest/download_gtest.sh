88074b656935230da8aeb2a6ab8773d3bf6b41fb
sberlato <sberlato@fbk.eu>
2022-03-23 10:27:14 +0100

    Fixed installation following https://github.com/ChantMisaya/openabe

--- deps/gtest/download_gtest.sh
+++ deps/gtest/download_gtest.sh
@@ -7,8 +7,13 @@ if [[ $VERSION = "" ]]; then
 fi
 
 GTEST_VERSION=googletest-release-${VERSION}.zip
-GTEST_LINK=https://github.com/google/googletest/archive/release-${VERSION}.zip
+
+GTEST_LINK=https://github.com/google/googletest/archive/refs/tags/release-${VERSION}.zip
+# GTEST_LINK=https://github.com/google/googletest/archive/release-${VERSION}.zip
 
 if [[ ! -f ${GTEST_VERSION} ]]; then
-    wget -O ${GTEST_VERSION} ${GTEST_LINK}
+
+    wget -O ${GTEST_VERSION} ${GTEST_LINK} --no-check-certificate
+    # wget -O ${GTEST_VERSION} ${GTEST_LINK}
+    
 fi
